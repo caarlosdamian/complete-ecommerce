1545447b27298099be39dd13ec57b3410031dc62
caarlosdamian <dapecarlos@gmail.com>
2021-10-07 16:18:00 -0500

    Fix/Api cors config

--- Client/src/components/Products/Products.jsx
+++ Client/src/components/Products/Products.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../../utils/data";
+import axios from "axios";
 import Product from "./Product/Product";
 const Container = styled.div`
   padding: 20px;
@@ -7,7 +9,22 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 `;
-const Products = ({ cate, filters, sort }) => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const token =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNTc5MmU5MWU2OWJiODdlMTNhZTA3YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzEyOTI0NSwiZXhwIjoxNjMzMzg4NDQ1fQ.ZbCkGVmClBRiRDLCFrbdwB-suvlnJvSSln23Xbs6Vvs";
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:5000/api/products",
+        );
+        console.log(res);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
   return (
     <Container>
       {popularProducts.map((item) => (
--- Server/index.js
+++ Server/index.js
@@ -15,15 +15,16 @@ moongose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DBConnection Successfull"))
   .catch((err) => console.log(err));
+
+app.use(cors());
 app.use(express.json());
-app.use(cors);
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderoute);
-app.use("/api/auth", authRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000, () => {
   console.log("Backend server running");
 });
