1b58a167347bb496bb5f1e97f801677ea8c7a827
caarlosdamian <dapecarlos@gmail.com>
2021-11-02 20:28:02 -0600

    Feat/ Add Login to Admin

--- Client/src/api/requestMethods.js
+++ Client/src/api/requestMethods.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNTc5MmU5MWU2OWJiODdlMTNhZTA3YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNDcwNTc2OCwiZXhwIjoxNjM0OTY0OTY4fQ.47L_j4ZU7prW3J68l7XUeZpMgui6njmIllD84fSw1IM";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+  ?.currentUser?.accessToken;
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
